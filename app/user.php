5804397cd948acf9bed61bab2c94b99fe0383782
Ganesh Karthikeyan <kgk@Ganeshs-MacBook-Air.local>
2018-02-18 21:14:11 +0530

    userModuleAndDBPatchesUpdated

--- app/user.php
+++ app/user.php
@@ -1,6 +1,6 @@
 <?php
 
-//////////////////READ ALL USER ////////////////////////////////
+/*READ ALL USER START*/
 
 $app->get('/allusers', function ($request, $response, $args) {
 	require 'config.php';
@@ -16,7 +16,7 @@ $app->get('/allusers', function ($request, $response, $args) {
 		$id = $_GET['id'];
 		$where .= " AND id=$id";
 	}
-	$UserView = "SELECT * from tbl_user $where ORDER BY id asc";
+	$UserView = "SELECT * from users $where ORDER BY id asc";
 
 	$resUsers = $mysqli->query($UserView);
 	if ($resUsers->num_rows > 0) {
@@ -31,7 +31,10 @@ $app->get('/allusers', function ($request, $response, $args) {
 	echo json_encode(array('status' => 'Success'));
 
 });
+/*READ ALL USER END*/
 
+
+/*CREATE USER WEB SERVICE START*/
 $app->post('/user', function ($request, $response, $args) {
 	require 'config.php';
 
@@ -39,21 +42,21 @@ $app->post('/user', function ($request, $response, $args) {
 	$post = json_decode($json, true);
 	$tok = $request->getAttribute($tok);
 	$token = json_decode($tok, true);
+	$userID = $token['userid']
 	$id = $post['id'];
 	extract($post);
-	$insertSql = "INSERT INTO tbl_user(firstname,lastname,username,password,email,age,gender,status,role_id,created_at) VALUES('$firstname','$lastname','$username','" . md5($password) . "','$email','$age','$gender','$status','$role_id',NOW())";
-	// echo $insertSql;die;
+	$insertSql = "INSERT INTO users(first_name,last_name,user_name,password,role,email,gender,status,created_at,created_by,modified_at,modified_by) VALUES('$first_name','$last_name','$user_name','" . md5($password) . "','$role','$email','$gender','$status',NOW(),$userid,NOW(),$userid)";
 	$resultSql = $mysqli->query($insertSql);
 	if ($resultSql) {
 		$post['id'] = $mysqli->insert_id;
-		return $this->response->withJson(array('user_created' => $post));
+		return $this->response->withJson(array('user_created' => $post,'status'=>'success','code'=>200));
 	} else {
-		return $this->response->withJson(array('error' => 'user not created'));
+		return $this->response->withJson(array('error' => 'user not created','status'=>'failed','code'=>400));
 	}
 
-	echo json_encode(array('status' => 'Success'));
-
 });
+/*CREATE USER WEB SERVICE END*/
+
 $app->put('/user/{id}', function ($request, $response, $args) {
 	require 'config.php';
 
